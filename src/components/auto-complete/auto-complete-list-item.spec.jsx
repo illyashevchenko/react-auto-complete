4381d4839220f05eccb3868eac728f04c69e4b3f
Illia Shevchenko <Illia_Shevchenko@epam.com>
2015-10-08 15:39:22 +0300

    1. Try to ad tests - not working. Will move to webpack first

---
+++ src/components/auto-complete/auto-complete-list-item.spec.jsx
@@ -0,0 +1,14 @@
+/**
+ * Created by Illia_Shevchenko on 08.10.2015.
+ */
+require('testdom')('<html><body></body></html>');
+
+var chai   = require('chai'),
+    expect = chai.expect,
+    AutoCompleteListItem = require('./auto-complete-list-item.jsx');
+
+describe('Test', () => {
+    it('should be true', () => {
+      expect(true).to.equal(true);
+    });
+});
